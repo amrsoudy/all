7dcef633257b0e378a47dcedbc8c12bc0e231829
Amr Seoudy <amrsoudy@github.com>
2017-11-15 11:31:23 -0500

    max with Math.max java

---
+++ getmaxwithMaxFunction.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+
+public static void main (string[] args)
+
+{
+	
+		Scanner kb = new Scanner() ;
+		System.out.print("input the first number ")
+		double x  = kb.nextDouble();
+		System.out.print("input the second number ")
+		double y  = kb.nextDouble();
+		System.out.print("input the third number ")
+		double z  = kb.nextDouble();
+		
+		System.out.print("the smallest number is " + smallest(x,y,z))
+		
+			
+		
+	}
+	
+	public static double smallest (double x,double y,double z)
+	{
+		
+		return  Math.min((Math.min(x, y),z);
+		
+	}
+
+
+}
+			
