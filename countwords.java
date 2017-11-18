31600e5d3ac245f6b985ed1da9a10704f6cbe0e5
Amr Seoudy <amrsoudy@github.com>
2017-11-18 11:22:13 -0500

    count worders
    depand on the split by "" and put it in list and count by  i  in for loop

---
+++ countwords.java
@@ -0,0 +1,33 @@
+package javapractice;
+
+import java.util.Scanner;
+
+public class ex5 {
+
+	public static void main(String[] args) {
+		
+	Scanner kb = new Scanner(System.in);
+	System.out.println("donner moi tout le phrase SVP  :  ");
+	String data = kb.nextLine();
+	String[] x = data.split(" ") ;
+	int i = 0;
+	
+	
+	
+	for( i=0 ; i <(x.length);i++)
+	{
+		
+		
+		
+	}
+	System.out.println(i);
+	
+
+	
+		
+		
+
+	}
+
+
+}
