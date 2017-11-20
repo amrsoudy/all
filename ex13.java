5ccc64f4d6c28bef72da69cd26a4b9f5ff5f2749
amrsoudy <32444508+amrsoudy@users.noreply.github.com>
2017-11-20 14:54:51 -0500

    update

--- ex13.java
+++ ex13.java
@@ -1,10 +1,8 @@
 // Write a Java program to sort a numeric array and a string array
 
-package excercie;
-
+package excercie13;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,44 +10,47 @@ public class example {
 
 	public static void main(String[] args) {
 		double somme =0 ;
-		
 		Scanner kb  = new Scanner(System.in);
-		ArrayList<Double> list = new ArrayList();
-		double i = 1;
+		
+		ArrayList<Double> mylist = new ArrayList<Double>();  
+		
+		double i = 1.0;
 		
 		while (i !=0){
 			
 			System.out.println("Write the price ");
 			
+		
 			i = kb.nextDouble();
+			
+			
 			if (i !=0){
-				list.add(i);
-
+				mylist.add(i);
 				
 			}
 						
 		}
-		for(Double n :list){
+		for(Double n :mylist){
 			
-				System.out.println("Produit"+(list.indexOf(n)+1)+"............"+n);
+				System.out.println("Produit"+(mylist.indexOf(n)+1)+"............"+n);
 				somme+=n;
-				System.out.println();
-				System.out.println();
-				System.out.println();
-				
-				System.out.println("Sous-total : "+somme);
-				System.out.println("TPS : "+somme*.05);
-
-
-			
-			
-			
-			
+	
 		}
-		
+        DecimalFormat formatter = new DecimalFormat("#.###");
 
 		
-
+		double tps = somme*0.05;
+		double tvq = somme * 0.09975;
+		double total = somme+tps+tvq;
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("Sous-total : "+formatter.format(somme)+"$");
+		System.out.println("TPS : "+formatter.format(tps)+"$");
+		System.out.println("TVQ : "+formatter.format(tvq)+"$");
+		System.out.println("total : "+formatter.format(total)+"$");
+	
 	}
 
 }
